0f6c86fca560bee4115ac6eb2a4ab25c44d73146
Dereje1 <Dereje1>
2017-09-23 11:08:33 -0400

    minor modifications

--- server.js
+++ server.js
@@ -18,6 +18,6 @@ app.get("/", function (request, response) {
 
 
 // listen for requests :)
-var listener = app.listen(3000, function () {
+var listener = app.listen(process.env.PORT || 3000, function () {
   console.log('Your app is listening on port ' + listener.address().port);
 });
