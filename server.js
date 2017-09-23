5f5b1a78f525de5b9d07c370b6ba1a21a6476289
Dereje1 <Dereje1>
2017-09-23 14:42:17 -0400

    change response --> res , more common in docs

--- server.js
+++ server.js
@@ -12,8 +12,8 @@ var app = express();
 app.use(express.static('public'));
 
 // http://expressjs.com/en/starter/basic-routing.html
-app.get("/", function (request, response) {
-  response.sendFile(__dirname + '/views/index.html');
+app.get("/", function (req, res) {
+  res.sendFile(__dirname + '/views/index.html');
 });
 
 
